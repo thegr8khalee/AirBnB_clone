40e77687c927244a3b20744c86bbca0abb573759
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-11 22:27:44 +0300

    mm

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from models.engine.file_storage import file_storage
-
-storage = file_storage
-storage.reload()
--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-class City(BaseModel):
-    """
-    City class inherits from BaseModel
-    """
-    state_id = ""
-    name = ""
