2d76f5a4eb6dd2d11a432953a55d041ad8a8ce60
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-12 20:04:48 +0300

    task 6

--- console.py
+++ console.py
@@ -9,8 +9,24 @@ class HBNBCommand(cmd.Cmd):
     """
     Command int
     """
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     valid_classes = ["BaseModel"]
 
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
     
+    def do_EOF(self, arg):
+        """
+        EOF reached
+        """
+        print()
+        return True
+    
+    def do_emptyline(self, arg):
+        """do nothing"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
