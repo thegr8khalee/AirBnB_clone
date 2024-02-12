6a1f1e3efca9b137d3fdc4df6be065d48e8e6b8b
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-12 20:21:19 +0300

    mm

--- console.py
+++ console.py
@@ -17,7 +17,8 @@ class HBNBCommand(cmd.Cmd):
     valid_classes = ["BaseModel"]
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program
+        """
         return True
     
     def do_EOF(self, arg):
