10dbd54aa578c04e5c5bfa36c3d4f88ba01c6613
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-02-13 18:52:17 +0300

    task 15

--- console.py
+++ console.py
@@ -266,12 +266,12 @@ class HBNBCommand(cmd.Cmd):
             objs = storage.all()
             if key not in objs:
                 print("** no instance found **")
-            
-            instance = objs[key]
+            else:
+                instance = objs[key]
 
-            setattr(instance, attr, attr_value)
-            storage.save()
-
+                setattr(instance, attr, attr_value)
+                storage.save()
+                
 
 
 if __name__ == '__main__':
