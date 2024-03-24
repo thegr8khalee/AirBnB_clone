8d72ceda4663e85072daf72f3a37cad017ab4c76
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-24 13:42:06 +0300

    mm

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,19 +2,21 @@
 import json
 import os
 
+
 class FileStorage:
     """_summary_
 
     Returns:
         _type_: _description_
     """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """returns the dictionary __objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
@@ -25,26 +27,24 @@ class FileStorage:
         serialized_objects = {}
         for key, value in self.__objects.items():
             serialized_objects[key] = value.to_dict()
-        
+
         with open(self.__file_path, "w", encoding="utf-8") as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
-        """ deserializes the JSON file to __objects (only if the JSON file (__file_path)"""
+        """deserializes the JSON file to __objects (only if the JSON file (__file_path)"""
         if os.path.isfile(FileStorage.__file_path):
-            try:
-                with open(self.__file_path, 'r', encoding="utc-8") as file:
-                    things = json.load(file)
-                    things = {key: self.classes()[value["__class__"]](**value)
-                        for key, value in things.items()}
-                    FileStorage.__objects = things
-            except Exception:
-                pass
+            with open(self.__file_path, "r", encoding="utc-8") as file:
+                things = json.load(file)
+                things = {
+                    key: self.classes()[value["__class__"]](**value)
+                    for key, value in things.items()
+                }
+                FileStorage.__objects = things
 
     def classes(self):
-    
         """Returns a dictionary of valid classes and their references."""
-    
+
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
@@ -53,11 +53,13 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
 
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+        }
         return classes
